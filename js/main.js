17efaf15e458ae15540e3fed5a343ac650ad634d
l0th3r <alfabetatango9@gmail.com>
2021-10-05 18:25:07 +0200

    init base

--- js/main.js
+++ js/main.js
@@ -1,31 +1,24 @@
 import * as THREE from 'three';
 import { MainScene } from './scenes.js';
 import { Objects, Object } from './objects';
-import { Vector3 } from 'three';
+import { GetTexture, Texture } from './texture';
 
 MainScene.Init();
 MainScene.Debug(); // DEBUG
 
-const moonTexture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/l0th3r/js3DTest/main/src/assets/moon.jpg');
-const sun = new THREE.Mesh(
-    new THREE.SphereGeometry(2, 32, 32),
-    new THREE.MeshStandardMaterial({
-        map: moonTexture
-    })
-);
-sun.position.set(0, 0, 0);
+const moon_t = new THREE.TextureLoader().load('https://raw.githubusercontent.com/l0th3r/js3DTest/main/src/assets/moon.jpg');
 
 const moon = new THREE.Mesh(
     new THREE.SphereGeometry(1, 32, 32),
     new THREE.MeshStandardMaterial({
-        map: moonTexture
+        map: moon_t
     })
 );
 
 const moon2 = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 32, 32),
     new THREE.MeshStandardMaterial({
-        map: moonTexture
+        map: moon_t
     })
 );
 
---
+++ js/texture.js
@@ -0,0 +1,33 @@
+import * as THREE from 'three';
+
+const Textures = [];
+
+class Texture {
+    name = "null";
+    txtr = undefined;
+    isReady = false;
+    ErrorHandler(err) {
+        console.warn('texture: ' + err.target.src + " haven't loaded...");
+    };
+    async LoadTexture() {
+        this.txtr = await new THREE.TextureLoader().load(this.path, ()=>{this.isReady = true}, undefined, this.ErrorHandler);
+    };
+    constructor(name, path) {
+        this.isReady = false;
+        Textures.push(this);
+        this.name = name;
+        this.path = path;
+        this.LoadTexture();
+    }
+}
+
+const GetTexture = function (name) {
+    const temp = Textures.filter(t => t.name === name);
+    console.log(temp[0].isReady);
+    if(temp[0].isReady)
+        return temp[0].txtr;
+    else
+        return undefined;
+}
+
+export {GetTexture, Texture};
